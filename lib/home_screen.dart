ea82d54bd38eb0796e49eded2f9905cdbd96a338
Mostak <mostakahmedshahad74@gmail.com>
2025-02-15 15:55:35 +0600

    google_map

---
+++ lib/home_screen.dart
@@ -0,0 +1,68 @@
+import 'dart:async';
+import 'dart:convert';
+
+import 'package:flutter/material.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
+//import 'package:google_maps_flutter_web/google_maps_flutter_web.dart';
+
+class HomeScreen extends StatefulWidget {
+  const HomeScreen({super.key});
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  Completer<GoogleMapController> _controller = Completer();
+  static final CameraPosition _kGooglePlex = const CameraPosition(
+    target: LatLng(24, 90),
+    zoom: 14.4746,
+  );
+
+  List<Marker> _marker = [];
+  final List<Marker> _list = const [
+    Marker(
+        markerId: MarkerId('1'),
+        position: LatLng(24, 90),
+        infoWindow: InfoWindow(title: 'My current location')),
+    Marker(
+        markerId: MarkerId('2'),
+        position: LatLng(23.85, 90.44),
+        infoWindow: InfoWindow(title: 'To go')),
+         Marker(
+        markerId: MarkerId('3'),
+        position: LatLng(24.36962000, 88.60748),
+        infoWindow: InfoWindow(title: 'New Location')),
+  ];
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    _marker.addAll(_list);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: GoogleMap(
+        initialCameraPosition: _kGooglePlex,
+        markers: Set<Marker>.of(_marker),
+        mapType: MapType.normal,
+        compassEnabled: false,
+        myLocationEnabled: true,
+        onMapCreated: (GoogleMapController controller) {
+          _controller.complete(controller);
+        },
+      ),
+      floatingActionButton: FloatingActionButton(onPressed: () async {
+        GoogleMapController controller = await _controller.future;
+        controller.animateCamera(CameraUpdate.newCameraPosition(
+            CameraPosition(target: LatLng(24.36962000, 88.60748), zoom: 14)));
+            setState(() {
+              
+            });
+      }),
+    );   
+  }
+}
